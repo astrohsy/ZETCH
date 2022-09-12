5b96e71cf6cc418205d6218a02361631c2ead4e2
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-09-11 21:38:16 -0400

    db: Add a sample db interaction

---
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // This means that this class is a Controller
+@RequestMapping(path="/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser(@RequestParam String name,
+                                           @RequestParam String email,
+                                           @RequestParam String username) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        ZetchUser n = new ZetchUser();
+        n.setName(name);
+        n.setEmail(email);
+        n.setUsername(username);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<ZetchUser> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
+    }
+}
