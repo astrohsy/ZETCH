ea7fab36eee552698d34a0f2e02456045638dda7
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-09-18 21:14:26 -0400

    Add tests for User object

---
+++ api_server/src/test/java/io/zetch/app/domain/UserTest.java
@@ -0,0 +1,25 @@
+package io.zetch.app.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.Is.*;
+
+public class UserTest {
+    @Test
+    public void testConstructorAndGetters() {
+        User u = new User("b123", "Bob", "bob@example.com");
+
+        assertThat(u.getUsername(), is("b123"));
+        assertThat(u.getName(), is("Bob"));
+        assertThat(u.getEmail(), is("bob@example.com"));
+    }
+
+    @Test
+    public void equalsHashcodeVerify() {
+        User u1 = new User("b123", "Bob", "bob@example.com");
+        User u2 = new User("b123", "Bob", "bob@example.com");
+
+        assertThat(u1, is(u2));
+        assertThat(u1.hashCode(), is(u2.hashCode()));
+    }
+}
