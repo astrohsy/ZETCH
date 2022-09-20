971d113e6dd65a61358df8f8c50e0e87f37c7fc4
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-09-19 21:12:38 -0400

    Change UserController to use UserDto

--- api_server/src/main/java/io/zetch/app/web/UserController.java
+++ api_server/src/main/java/io/zetch/app/web/UserController.java
@@ -2,13 +2,17 @@ package io.zetch.app.web;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.zetch.app.domain.Restaurant;
 import io.zetch.app.domain.User;
+import io.zetch.app.domain.UserDto;
 import io.zetch.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -23,27 +27,37 @@ public class UserController {
 
     @GetMapping(path="/")
     @Operation(summary = "Retrieve all users")
-    @ResponseBody Iterable<User> getAllUsers() {
-        return userService.getAll();
+    @ResponseBody Iterable<UserDto> getAllUsers() {
+        return userService.getAll().stream().map(this::toDto).collect(Collectors.toList());
     }
 
     @PostMapping(path="/")
     @Operation(summary = "Create a new user")
-    @ResponseBody String addNewUser(@RequestBody User newUser) {
-        userService.createNew(newUser);
+    @ResponseBody String addNewUser(@RequestBody UserDto newUserDto) {
+        userService.createNew(newUserDto.getUsername(), newUserDto.getName(), newUserDto.getEmail());
         return "User saved";
     }
 
     @GetMapping("/{username}")
     @Operation(summary = "Retrieve a single user")
-    User getOneUser(@PathVariable String username) {
-        return userService.getOne(username);
+    UserDto getOneUser(@PathVariable String username) {
+        return toDto(userService.getOne(username));
     }
 
     @PutMapping("/{username}")
     @Operation(summary = "Modify user attributes")
-    User updateUser(@RequestBody User newUser, @PathVariable String username) {
-        return userService.update(newUser, username);
+    UserDto updateUser(@RequestBody UserDto newUserDto, @PathVariable String username) {
+        return toDto(userService.update(username, newUserDto.getName(), newUserDto.getEmail()));
+    }
+
+    /**
+     * Convert the User entity to a User data transfer object
+     *
+     * @param user User to convert
+     * @return User DTO
+     */
+    private UserDto toDto(User user) {
+        return new UserDto(user.getUsername(), user.getName(), user.getEmail());
     }
 
     /**
