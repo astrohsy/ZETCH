b085f4f8e4b80795f75452c76882d344c7bf5e5f
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-10-01 21:19:56 -0400

    Update to use Cognito

--- api_server/src/main/java/io/zetch/app/web/HelloController.java
+++ api_server/src/main/java/io/zetch/app/web/HelloController.java
@@ -2,12 +2,26 @@ package io.zetch.app.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 @RestController
 public class HelloController {
 
   @GetMapping("/")
-  public String index() {
+  public String index1() {
     return "Greetings from Spring Boot!";
+  }
+
+  @GetMapping("/public")
+  public String index2() {
+    return "Greetings from Spring Boot! This is public";
+  }
+
+  @GetMapping("/private")
+  public String index3(JwtAuthenticationToken principal) {
+    String username = principal.getToken().getClaimAsString("cognito:username");
+    String group = principal.getToken().getClaimAsString("custom:affiliation");
+
+    return String.format("Greetings from Spring Boot, %s! You are in group %s", username, group);
   }
 }
