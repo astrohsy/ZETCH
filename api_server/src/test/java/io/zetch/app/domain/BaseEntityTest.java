0d7a6bbe11177f2de4018d867359d7125c996774
Hoseung Lee <astrohsy@gmail.com>
2022-10-03 18:21:48 -0400

    test: add BaseEntityTest

---
+++ api_server/src/test/java/io/zetch/app/domain/BaseEntityTest.java
@@ -0,0 +1,31 @@
+package io.zetch.app.domain;
+
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class BaseEntityTest {
+
+  private static final String USERNAME = "bob";
+  private static final String NAME = "Bob";
+  private static final String EMAIL = "bob@example.com";
+
+  @AllArgsConstructor
+  public class ConcreteEntity extends BaseEntity {
+    private String name;
+  }
+
+  @Test
+  public void testEquals() {
+    ConcreteEntity entityA = new ConcreteEntity("concreteA");
+    ConcreteEntity entityB = new ConcreteEntity("concreteB");
+
+    assertEquals(true, entityA.equals(entityA));
+    assertEquals(false, entityA.equals(null));
+    assertEquals(false, entityA.equals(entityB));
+    assertEquals(false, entityA.equals(10));
+  }
+}
