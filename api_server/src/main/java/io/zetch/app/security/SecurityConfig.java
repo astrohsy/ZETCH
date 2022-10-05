158f3ee071959cc83596b014bc6d5b570226c703
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-10-04 20:21:38 -0400

    Replace Auth0 config with Cognito

--- api_server/src/main/java/io/zetch/app/security/SecurityConfig.java
+++ api_server/src/main/java/io/zetch/app/security/SecurityConfig.java
@@ -1,5 +1,5 @@
 /**
- * This code is mostly from the Auth0 documentation:
+ * This code is mostly from the Auth0 documentation with modifications for Amazon Cognito:
  * https://auth0.com/docs/quickstart/backend/java-spring-security5/interactive
  */
 package io.zetch.app.security;
@@ -22,49 +22,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-  @Value("${auth0.audience}")
-  private String audience;
-
-  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-  private String issuer;
-
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     /*
-    This is where we configure the security required for our endpoints and setup our app to serve as
+    This is where we configure the security required for our endpoints and set up our app to serve as
     an OAuth2 Resource Server, using JWT validation.
     */
-    http.authorizeRequests()
-        .anyRequest()
-        .permitAll()
-        .and()
-        .cors()
-        .and()
+    http.cors().and().csrf().disable();
+
+    http.authorizeRequests(
+            authReqCustomizer ->
+                authReqCustomizer.antMatchers("/private").authenticated().anyRequest().permitAll())
         .oauth2ResourceServer()
         .jwt();
 
-    http.csrf().disable();
-
     return http.build();
-  }
-
-  @Bean
-  JwtDecoder jwtDecoder() {
-    /*
-    By default, Spring Security does not validate the "aud" claim of the token, to ensure that this token is
-    indeed intended for our app. Adding our own validator is easy to do:
-    */
-
-    NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
-
-    OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-    OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-    OAuth2TokenValidator<Jwt> withAudience =
-        new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
-    jwtDecoder.setJwtValidator(withAudience);
-
-    return jwtDecoder;
   }
 }
