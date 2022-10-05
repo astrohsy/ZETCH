b2eb2ad25b06bc31655a1d429ee220f44b744a6e
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-10-04 20:22:08 -0400

    Add sample public and private routes

--- api_server/src/main/java/io/zetch/app/web/HelloController.java
+++ api_server/src/main/java/io/zetch/app/web/HelloController.java
@@ -1,5 +1,6 @@
 package io.zetch.app.web;
 
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
   @GetMapping("/")
-  public String index() {
+  public String index1() {
     return "Greetings from Spring Boot!";
+  }
+
+  @GetMapping("/public")
+  public String index2() {
+    return "Greetings from Spring Boot! This is public.";
+  }
+
+  @GetMapping("/private")
+  public String index3(JwtAuthenticationToken principal) {
+    String username = principal.getToken().getClaimAsString("username");
+
+    return String.format("Greetings from Spring Boot. You are %s!", username);
   }
 }
