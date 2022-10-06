0fa73d16303f5621f6f687f9915ca2a1256d2c33
Hoseung Lee <astrohsy@gmail.com>
2022-10-06 11:06:54 -0400

    Fix

--- api_server/src/main/java/io/zetch/app/domain/review/ReviewDto.java
+++ api_server/src/main/java/io/zetch/app/domain/review/ReviewDto.java
@@ -1,13 +1,16 @@
 package io.zetch.app.domain.review;
 
 import java.io.Serializable;
+import io.zetch.app.domain.restaurant.RestaurantDto;
+import io.zetch.app.domain.user.UserDto;
 import lombok.ToString;
 import lombok.Value;
 
 @Value
-public class UserDto implements Serializable {
+public class ReviewDto implements Serializable {
   @ToString.Exclude Long id;
-  String username;
-  String name;
-  String email;
+  String comment;
+  Integer rating;
+  UserDto user;
+  RestaurantDto restaurant;
 }
--- api_server/src/main/java/io/zetch/app/domain/review/ReviewEntity.java
+++ api_server/src/main/java/io/zetch/app/domain/review/ReviewEntity.java
@@ -3,12 +3,12 @@ package io.zetch.app.domain.review;
 
 import io.zetch.app.domain.BaseEntity;
 import io.zetch.app.domain.restaurant.RestaurantEntity;
-import java.util.List;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import lombok.*;
+import io.zetch.app.domain.user.UserEntity;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -18,17 +18,11 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "zetch_user") // "User" is a reserved keyword in some DBs
-public class UserEntity extends BaseEntity {
-  @NonNull
-  @Column(unique = true)
-  private String username;
+@Table
+public class ReviewEntity extends BaseEntity {
+  private String comment;
+  private Integer rating;
 
-  private String name;
-  private String email;
-
-  @ManyToMany(
-      mappedBy =
-          "owners") // A restaurant might have multiple owners; a User might own multiplerestaurants
-  private List<RestaurantEntity> ownedRestaurants;
+  @ManyToOne private UserEntity user;
+  @ManyToOne private RestaurantEntity restaurant;
 }
