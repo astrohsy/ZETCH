a905f42283c284851c9b4a6aa3d81a0ead34c1ca
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-10-08 23:55:24 -0400

    Add more tests

---
+++ api_server/src/test/java/io/zetch/app/domain/AffiliationTest.java
@@ -0,0 +1,27 @@
+package io.zetch.app.domain;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import io.zetch.app.domain.user.Affiliation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AffiliationTest {
+
+  @ParameterizedTest
+  @CsvSource({"student, STUDENT", "faculty, FACULTY", "other, OTHER", "admin, ADMIN"})
+  public void fromString(String text, Affiliation affiliation) {
+    assertThat(Affiliation.fromString(text), is(affiliation));
+  }
+
+  @Test
+  public void fromString_Failure() {
+    assertThrows(IllegalArgumentException.class, () -> Affiliation.fromString("doesNotExist"));
+  }
+}
