c65597e164499fccd7b6776594b72f125f5c5728
Hoseung Lee <astrohsy@gmail.com>
2022-10-16 11:04:57 -0400

    Update LocationController.java

--- api_server/src/main/java/io/zetch/app/web/LocationController.java
+++ api_server/src/main/java/io/zetch/app/web/LocationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/locations")
 @Tag(name = "Locations")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*") // NOSONAR
 public class LocationController {
   private final LocationService locationService;
 
