cb33ae391c278643f9da06e2679f6f385360f891
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-10-20 05:59:48 -0400

    bug: fix duplicate displayName setting in User seeding (#72)

--- api_server/src/main/java/io/zetch/app/seed/SeedUsers.java
+++ api_server/src/main/java/io/zetch/app/seed/SeedUsers.java
@@ -10,35 +10,35 @@ public class SeedUsers {
           .username("admin")
           .affiliation(Affiliation.ADMIN)
           .displayName("Admin.")
-          .displayName(null)
+          .email(null)
           .build();
   private static final UserEntity USER_2 =
       UserEntity.builder()
           .username("bob")
           .affiliation(Affiliation.STUDENT)
           .displayName("Bob R.")
-          .displayName("bob@me.com")
+          .email("bob@me.com")
           .build();
   private static final UserEntity USER_3 =
       UserEntity.builder()
           .username("cat")
           .affiliation(Affiliation.STUDENT)
           .displayName("Kat F.")
-          .displayName("kat@kat.com")
+          .email("kat@kat.com")
           .build();
   private static final UserEntity USER_4 =
       UserEntity.builder()
           .username("amy")
           .affiliation(Affiliation.FACULTY)
           .displayName("Amy T.")
-          .displayName("amy@dog.com")
+          .email("amy@dog.com")
           .build();
   private static final UserEntity USER_5 =
       UserEntity.builder()
           .username("sam")
           .affiliation(Affiliation.OTHER)
           .displayName("Sam 2.")
-          .displayName("sam@from.com")
+          .email("sam@from.com")
           .build();
 
   public static final List<UserEntity> USERS = List.of(USER_1, USER_2, USER_3, USER_4, USER_5);
