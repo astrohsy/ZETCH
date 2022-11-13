e2bea036b6568307b1f0a1aa080944dd7edab78d
Eugene Kim <eugene7kim@gmail.com>
2022-11-13 11:43:10 -0500

    tests: add UserService integration tests (#121)
    Co-authored-by: e7kim <e7kim@DESKTOP-4Q7RFN0.localdomain>
    Co-authored-by: github-actions <>

--- api_server/src/test/java/io/zetch/app/service/UserServiceIntegrationTest.java
+++ api_server/src/test/java/io/zetch/app/service/UserServiceIntegrationTest.java
@@ -1,6 +1,7 @@
 package io.zetch.app.service;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.hamcrest.core.Is.is;
 
@@ -8,6 +9,8 @@ import io.zetch.app.domain.user.Affiliation;
 import io.zetch.app.domain.user.UserEntity;
 import io.zetch.app.repo.UserRepository;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class UserServiceIntegrationTest {
 
+  private static final String NAME_1 = "cat";
+  private static final String NAME_2 = "dog";
+  UserEntity u1 =
+      UserEntity.builder()
+          .username(NAME_1)
+          .displayName("Cat")
+          .email("cat@cat.cat")
+          .affiliation(Affiliation.OTHER)
+          .build();
+  UserEntity u2 =
+      UserEntity.builder()
+          .username(NAME_2)
+          .displayName("Dog")
+          .email("dog@dog.dog")
+          .affiliation(Affiliation.OTHER)
+          .build();
   @Autowired private UserService userService;
   @Autowired private UserRepository userRepository;
 
@@ -26,28 +45,71 @@ class UserServiceIntegrationTest {
 
   @Test
   void getAll() {
-    UserEntity u1 =
-        UserEntity.builder()
-            .username("cat")
-            .displayName("Cat")
-            .email("cat@cat.cat")
-            .affiliation(Affiliation.OTHER)
-            .build();
     userRepository.save(u1);
-
+    userRepository.save(u2);
     List<UserEntity> users = userService.getAll();
-    assertThat(users.size(), is(1));
+    assertThat(users.size(), is(2));
     assertThat(users.get(0), is(u1));
+    assertThat(users.get(1), is(u2));
   }
 
   @Test
-  void create() {
-    UserEntity newUser = userService.createNew("bob", "bob", "bob@me.com", "other");
-    List<UserEntity> users = userService.getAll();
-    assertThat(users.size(), is(1));
+  void getOne() {
+    userRepository.save(u1);
+    UserEntity user = userService.getOne(NAME_1);
+    assertThat(user, is(u1));
+  }
+
+  @Test
+  void getOneFails() {
+    userRepository.save(u1);
+    assertThrows(NoSuchElementException.class, () -> userService.getOne(NAME_2));
+  }
+
+  @Test
+  void createNew() {
+    UserEntity newUser = userService.createNew("bob", "Bob", "bob@me.com", "other");
+    assertThat(userService.getAll().size(), is(1));
     assertThat(newUser.getUsername(), is("bob"));
-    assertThat(newUser.getDisplayName(), is("bob"));
+    assertThat(newUser.getDisplayName(), is("Bob"));
     assertThat(newUser.getEmail(), is("bob@me.com"));
     assertThat(newUser.getAffiliation(), is(Affiliation.OTHER));
+  }
+
+  @Test
+  void createNewFails() {
+    userRepository.save(u1);
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> userService.createNew(NAME_1, "Bob", "bob@me.com", "other"));
+  }
+
+  @Test
+  void update() {
+    userRepository.save(u1);
+    UserEntity newUser = userService.update(NAME_1, "Bob", "bob@me.com", "other");
+    assertThat(userService.getAll().size(), is(1));
+    assertThat(newUser.getUsername(), is(NAME_1));
+    assertThat(newUser.getDisplayName(), is("Bob"));
+    assertThat(newUser.getEmail(), is("bob@me.com"));
+    assertThat(newUser.getAffiliation(), is(Affiliation.OTHER));
+  }
+
+  @Test
+  void updateFails() {
+    assertThrows(
+        NoSuchElementException.class,
+        () -> userService.update(NAME_1, "Bob", "bob@me.com", "other"));
+  }
+
+  @Test
+  void delete() {
+    userRepository.save(u1);
+    assertThat(userService.delete(NAME_1), is(u1));
+  }
+
+  @Test
+  void deleteFails() {
+    assertThrows(NoSuchElementException.class, () -> userService.delete(NAME_1));
   }
 }
