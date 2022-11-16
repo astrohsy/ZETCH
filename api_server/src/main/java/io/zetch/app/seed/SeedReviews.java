626297ae39e8361c6dc3f3f46f1436f6f2ecd126
Zikora Agbapu <69334935+zagbapu@users.noreply.github.com>
2022-11-16 12:00:22 -0500

    feat: additional seed for demo (#123)
    Co-authored-by: Eugene Kim <eugene7kim@gmail.com>
    Co-authored-by: e7kim <e7kim@DESKTOP-4Q7RFN0.localdomain>
    Co-authored-by: github-actions <>

--- api_server/src/main/java/io/zetch/app/seed/SeedReviews.java
+++ api_server/src/main/java/io/zetch/app/seed/SeedReviews.java
@@ -6,10 +6,6 @@ import java.util.List;
 /** Helper class for adding sample reviews. */
 public class SeedReviews {
 
-  private SeedReviews() {
-    throw new IllegalStateException("Utility class");
-  }
-
   public static final ReviewEntity REVIEW1 =
       ReviewEntity.builder()
           .comment("Great meal, I enjoyed the bacon and ranch pizza.")
@@ -45,6 +41,186 @@ public class SeedReviews {
           .user(SeedUsers.USER5)
           .location(SeedLocations.LOCATION4)
           .build();
+
+  //  5-Stars
+  public static final ReviewEntity REVIEW6 =
+      ReviewEntity.builder()
+          .comment(
+              "I love the activities and interaction that they offered. "
+                  + "I like this museum better than Guggenheim.")
+          .rating(5)
+          .user(SeedUsers.USER1)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW7 =
+      ReviewEntity.builder()
+          .comment(
+              "My new favorite art museum. There’s about 6 levels and over "
+                  + "400+ rooms of art here from sculptures, paintings, to multi-media")
+          .rating(5)
+          .user(SeedUsers.USER3)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW8 =
+      ReviewEntity.builder()
+          .comment(
+              "Came here on a Saturday for my birthday weekend and had a great time! "
+                  + "The museum is so spacious that even on a busy day, "
+                  + "you have your own space to move around and admire the artwork.")
+          .rating(5)
+          .user(SeedUsers.USER4)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW9 =
+      ReviewEntity.builder()
+          .comment(
+              "Overall it's a good museum but there are a few "
+                  + "things that can be improved for a better experience of visitors.")
+          .rating(5)
+          .user(SeedUsers.USER5)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW10 =
+      ReviewEntity.builder()
+          .comment(
+              "Fantastic collection, audio guides of most major pieces "
+                  + "of art available on your phone, and free WiFi to "
+                  + "access it without any restrictions.")
+          .rating(5)
+          .user(SeedUsers.USER6)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW11 =
+      ReviewEntity.builder()
+          .comment(
+              "Excellent exhibits, can spend all day covering wide range!"
+                  + " Go back again and again to enjoy new exhibits periodically. ")
+          .rating(5)
+          .user(SeedUsers.USER7)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW12 =
+      ReviewEntity.builder()
+          .comment(
+              "Wonderful exhibition of art in its different times, "
+                  + "you have 5 floors of a wide variety of works by great artists."
+                  + " It is easy to walk between the different rooms"
+                  + " as they are all connected to each other.")
+          .rating(5)
+          .user(SeedUsers.USER8)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW13 =
+      ReviewEntity.builder()
+          .comment(
+              "An essential Manhattan destination. "
+                  + "The recent renovations made it brighter."
+                  + " Supreme people watching. "
+                  + "Temporary exhibitions tend to tell good stories")
+          .rating(4)
+          .user(SeedUsers.USER9)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  // 4 Stars
+  public static final ReviewEntity REVIEW14 =
+      ReviewEntity.builder()
+          .comment(
+              "Some of the pieces were very outlandish and thought provoking,"
+                  + " while others were mundane yet done in such a way that"
+                  + " makes you rethink your own realities.")
+          .rating(4)
+          .user(SeedUsers.USER10)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW15 =
+      ReviewEntity.builder()
+          .comment(
+              "It was worth it. Art works from amazing and highly recognized artists,"
+                  + " hundreds of different paintings to see… I really enjoyed it,"
+                  + " I can even say one of the best art museums in new York. ")
+          .rating(4)
+          .user(SeedUsers.USER11)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW16 =
+      ReviewEntity.builder()
+          .comment(
+              "Head straight to the top floor! Famous stuff is here,"
+                  + " and it’s get more modern as you descend floors. "
+                  + "Beautiful building, nice collections,"
+                  + " but almost all the best one and "
+                  + "so the crowds are concentrated at the top. ")
+          .rating(4)
+          .user(SeedUsers.USER12)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  // 3 Star
+  public static final ReviewEntity REVIEW17 =
+      ReviewEntity.builder()
+          .comment(
+              "The first Friday of the month is free for local "
+                  + "New Yorkers to visit but you need to book the"
+                  + " tickets in advance. Also they have free pen and "
+                  + "paper and even chairs for visitors to participate drawing. ")
+          .rating(3)
+          .user(SeedUsers.USER13)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW18 =
+      ReviewEntity.builder()
+          .comment(
+              "The first Friday of the month is free for local "
+                  + "New Yorkers to visit but you need to book the"
+                  + " tickets in advance. Also they have free pen and "
+                  + "paper and even chairs for visitors to participate drawing. ")
+          .rating(3)
+          .user(SeedUsers.USER14)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW19 =
+      ReviewEntity.builder()
+          .comment(
+              "Other the seeing Starry Night up close, the monet painting, "
+                  + "and a painting by Edward Hopper there is "
+                  + "not much else interesting to look at here. "
+                  + "Everything else in the museum is tasteless modern art. ")
+          .rating(2)
+          .user(SeedUsers.USER15)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW20 =
+      ReviewEntity.builder()
+          .comment(
+              "It was very disappointing that they old us tickets "
+                  + "for the same evening (first Friday of the month) "
+                  + "that it was a pay what you wish hour. The museum was crazy busy. ")
+          .rating(2)
+          .user(SeedUsers.USER16)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  // 1 Star
   public static final List<ReviewEntity> REVIEWS =
-      List.of(REVIEW1, REVIEW2, REVIEW3, REVIEW4, REVIEW5);
+      List.of(
+          REVIEW1, REVIEW2, REVIEW3, REVIEW4, REVIEW5, REVIEW6, REVIEW7, REVIEW8, REVIEW9, REVIEW10,
+          REVIEW11, REVIEW12, REVIEW13, REVIEW14, REVIEW15, REVIEW16, REVIEW17, REVIEW18, REVIEW19,
+          REVIEW20);
+
+  private SeedReviews() {
+    throw new IllegalStateException("Utility class");
+  }
 }
