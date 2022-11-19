9075c08f7f1c42fee044d06062858b6ac68caf30
Hoseung Lee <astrohsy@gmail.com>
2022-11-19 15:49:51 -0500

    fix styles

--- api_server/src/main/java/io/zetch/app/web/ReviewController.java
+++ api_server/src/main/java/io/zetch/app/web/ReviewController.java
@@ -8,14 +8,12 @@ import io.zetch.app.domain.review.ReviewGetDto;
 import io.zetch.app.domain.review.ReviewPostDto;
 import io.zetch.app.service.ReviewService;
 import java.util.Optional;
-
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 /** Controller for the review endpoints. */
 @RestController
