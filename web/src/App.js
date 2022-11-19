842623c8a166537f3c37d44fc6f1acd9a3669850
cshen4 <cshen4@wellesley.edu>
2022-11-19 14:49:20 -0500

    client: web frontend (#132)

---
+++ web/src/App.js
@@ -0,0 +1,124 @@
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Typography from '@mui/material/Typography';
+import './App.css';
+import Information from './components/Information';
+import Rating from './components/Rating';
+import Reviews from './components/Reviews';
+import Competitors from './components/Competitors';
+import { getAuthToken, getCognitoUser, getMuseumByName, getMyLocations, getUser } from './utils/apiCalls';
+import getParameterByName from './utils/getParameterByName';
+
+import { useEffect, useState } from 'react';
+
+
+
+const clientId = process.env.REACT_APP_COGNITO_CLIENT_ID;
+const redirect_uri = "http%3A%2F%2Flocalhost%3A3000"
+const authorize_url = `https://zetch-app-4.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=${clientId}&redirect_uri=${redirect_uri}`;
+
+function App() {
+
+  const accessToken = sessionStorage.getItem("access_token");
+
+  const [user, setUser] = useState(null);
+  const [locations, setLocations] = useState(null);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+
+  useEffect(() => {
+    const code = getParameterByName("code");
+    if (code) {
+      const getToken = async () => {
+        const res = await getAuthToken(code);
+
+        if (res.access_token) {
+          sessionStorage.setItem("access_token", res.access_token);
+          window.location.href = "http://localhost:3000";
+        }
+
+
+      }
+      getToken();
+    }
+  }, []);
+
+  useEffect(() => {
+    const initUser = async () => {
+      try {
+        const { Username: username } = await getCognitoUser(accessToken);
+        const user = await getUser(username);
+        const locations = await getMyLocations();
+
+        setUser(user);
+        setLocations(locations);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    if (accessToken) {
+      initUser();
+    }
+  }, [accessToken]);
+
+  const handleLocationChange = async (event) => {
+    const res = await getMuseumByName(event.target.value);
+    setSelectedLocation(res);
+  }
+
+  const renderSelectLocation = () => {
+    return <FormControl fullWidth style={{paddingBottom: "10%"}}>
+      <InputLabel id="select-loation-label">Select Museum</InputLabel>
+      <Select
+        labelId="select-location-label"
+        id="select-location"
+        value={selectedLocation ? selectedLocation.name : ''}
+        label="Select Museum"
+        onChange={handleLocationChange}
+      >
+        {locations.map((location) => {
+          return <MenuItem value={location.name}>{location.name}</MenuItem>
+        })}
+
+      </Select>
+    </FormControl>
+  }
+
+  const onEditLocation = async (locationName) => {
+    const res = await getMuseumByName(locationName);
+    setSelectedLocation(res);
+  }
+
+  return (
+    <div className="App" style={{ textAlign: 'left' }}>
+      <Box sx={{ width: '80%', padding: '10%' }}>
+        <Typography variant="h3" gutterBottom>
+          Museum Manager App
+        </Typography>
+        {
+          locations ? renderSelectLocation() : null
+        }
+        {selectedLocation ?
+          [<Information location={selectedLocation} onEditLocation={onEditLocation} />,
+          <Rating location={selectedLocation} />,
+          <Reviews location={selectedLocation} user={user} />] : null}
+        {!accessToken ? <Button variant="contained" onClick={() => { window.location.href = authorize_url; }}>Login</Button> : null}
+
+        {locations ? <><Competitors myLocations={locations} /><hr /></> : null}
+
+        {
+          user ?
+            <Typography variant="body1" gutterBottom>Logged in as: {user.display_name}</Typography> :
+            null
+        }
+        {accessToken ? <Button variant="contained" onClick={() => { sessionStorage.removeItem("access_token"); window.location.reload(); }}>Logout</Button> : null}
+      </Box>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/utils/apiCalls.js
@@ -0,0 +1,175 @@
+const clientId = process.env.REACT_APP_COGNITO_CLIENT_ID;
+
+
+const getJsonHeaders = () => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json; charset=utf-8");
+    headers.append("Accept", 'application/json');
+    return headers;
+}
+const request = async (url, requestOptions, isRawUrl = false) => {
+    if (!isRawUrl) {
+        url = "https://zetch.tech/" + url;
+
+        if (requestOptions.headers) {
+            requestOptions.headers.append("Authorization", `Bearer ${sessionStorage.getItem("access_token")}`);
+        } else {
+            const headers = new Headers();
+            headers.append("Authorization", `Bearer ${sessionStorage.getItem("access_token")}`);
+            requestOptions.headers = headers;
+        }
+    }
+    try {
+        const res = await fetch(url, requestOptions);
+        const response = await res.json()
+        const returned = await JSON.parse(JSON.stringify(response));
+        // console.log('Success', returned);
+        return await returned;
+    } catch (err) {
+        console.log("Error: " + err, "input: ", { url, ...requestOptions });
+    }
+}
+
+export async function getAuthToken(code) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+    const requestOptions = {
+        method: 'POST',
+        headers
+    };
+
+    return request(`https://zetch-app-4.auth.us-east-1.amazoncognito.com/oauth2/token?grant_type=authorization_code&client_id=${clientId}&redirect_uri=http%3A%2F%2Flocalhost%3A3000&code=${code}`, requestOptions, true);
+}
+
+export async function getCognitoUser(accessToken) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-amz-json-1.1");
+    headers.append("X-Amz-Target", "AWSCognitoIdentityProviderService.GetUser");
+    headers.append("Content-Length", "1162");
+    const body = { "AccessToken": accessToken }
+    const requestOptions = {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(body)
+    };
+
+    return request(`https://cognito-idp.us-east-1.amazonaws.com/`, requestOptions, true);
+}
+
+export async function getUser(username) {
+    const requestOptions = {
+        method: 'GET',
+    };
+
+    const url = `users/${encodeURI(username)}`;
+
+    return request(url, requestOptions);
+}
+
+export async function getMyLocations() {
+    const requestOptions = {
+        method: 'GET',
+    };
+
+    const url = `locations/mine`;
+
+    return request(url, requestOptions);
+}
+
+export async function getMuseums() {
+    const requestOptions = {
+        method: 'GET',
+    };
+
+    const url = `locations/search?type=museum`;
+
+    return request(url, requestOptions);
+}
+
+export async function getMuseumByName(name) {
+
+
+    const requestOptions = {
+        method: 'GET',
+    };
+
+    const url = `locations/${encodeURI(name)}`;
+
+    return request(url, requestOptions);
+}
+
+export async function updateMuseum(body) {
+    const name = body.name;
+    delete body.name
+
+    const requestOptions = {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: getJsonHeaders()
+    };
+
+    const url = `locations/${encodeURI(name)}`;
+
+    return request(url, requestOptions);
+}
+
+export async function getAverageRatingOfMuseum(museum) {
+
+    const requestOptions = {
+        method: 'GET'
+    };
+
+    const url = `locations/${encodeURI(museum)}/averageRating`;
+
+    return request(url, requestOptions);
+}
+
+export async function getRatingHistogramOfMuseum(museum) {
+
+    const requestOptions = {
+        method: 'GET'
+    };
+
+    const url = `locations/${encodeURI(museum)}/ratingHistogram`;
+
+    return request(url, requestOptions);
+}
+
+export async function getReviewsForMuseum(museumId) {
+
+    const requestOptions = {
+        method: 'GET'
+    };
+
+    const url = `reviews?locationId=${museumId}`;
+
+    return request(url, requestOptions);
+}
+
+export async function replyToReview(reviewId, userId, comment) {
+    const body = {
+        review_id: reviewId,
+        user_id: userId,
+        reply_comment: comment
+    }
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: getJsonHeaders()
+    };
+
+    const url = `replies/`;
+
+    return request(url, requestOptions);
+}
+
+export async function getRepliesToReview(reviewId) {
+
+    const requestOptions = {
+        method: 'GET'
+    };
+
+    const url = `replies/review/${reviewId}`;
+
+    return request(url, requestOptions);
+}
