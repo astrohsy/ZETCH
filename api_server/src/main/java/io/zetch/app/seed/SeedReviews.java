a8d6defe24efe48e1075f212272fdd0bbf930aa9
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-12-18 18:32:30 -0500

    client: add more reviews for Columbia Museum (#160)

--- api_server/src/main/java/io/zetch/app/seed/SeedReviews.java
+++ api_server/src/main/java/io/zetch/app/seed/SeedReviews.java
@@ -53,16 +53,6 @@ public class SeedReviews {
           .location(SeedLocations.LOCATION5)
           .build();
 
-  public static final ReviewEntity REVIEW7 =
-      ReviewEntity.builder()
-          .comment(
-              "My new favorite art museum. There’s about 6 levels and over "
-                  + "400+ rooms of art here from sculptures, paintings, to multi-media")
-          .rating(5)
-          .user(SeedUsers.USER3)
-          .location(SeedLocations.LOCATION6)
-          .build();
-
   public static final ReviewEntity REVIEW8 =
       ReviewEntity.builder()
           .comment(
@@ -93,16 +83,6 @@ public class SeedReviews {
           .rating(5)
           .user(SeedUsers.USER6)
           .location(SeedLocations.LOCATION5)
-          .build();
-
-  public static final ReviewEntity REVIEW11 =
-      ReviewEntity.builder()
-          .comment(
-              "Excellent exhibits, can spend all day covering wide range!"
-                  + " Go back again and again to enjoy new exhibits periodically. ")
-          .rating(5)
-          .user(SeedUsers.USER7)
-          .location(SeedLocations.LOCATION6)
           .build();
 
   public static final ReviewEntity REVIEW12 =
@@ -139,17 +119,6 @@ public class SeedReviews {
           .rating(4)
           .user(SeedUsers.USER10)
           .location(SeedLocations.LOCATION5)
-          .build();
-
-  public static final ReviewEntity REVIEW15 =
-      ReviewEntity.builder()
-          .comment(
-              "It was worth it. Art works from amazing and highly recognized artists,"
-                  + " hundreds of different paintings to see… I really enjoyed it,"
-                  + " I can even say one of the best art museums in new York. ")
-          .rating(4)
-          .user(SeedUsers.USER11)
-          .location(SeedLocations.LOCATION6)
           .build();
 
   public static final ReviewEntity REVIEW16 =
@@ -190,18 +159,6 @@ public class SeedReviews {
           .location(SeedLocations.LOCATION5)
           .build();
 
-  public static final ReviewEntity REVIEW19 =
-      ReviewEntity.builder()
-          .comment(
-              "Other the seeing Starry Night up close, the monet painting, "
-                  + "and a painting by Edward Hopper there is "
-                  + "not much else interesting to look at here. "
-                  + "Everything else in the museum is tasteless modern art. ")
-          .rating(2)
-          .user(SeedUsers.USER15)
-          .location(SeedLocations.LOCATION6)
-          .build();
-
   public static final ReviewEntity REVIEW20 =
       ReviewEntity.builder()
           .comment(
@@ -234,12 +191,195 @@ public class SeedReviews {
           .location(SeedLocations.LOCATION5)
           .build();
 
-  // 1 Star
+  // Columbia Museum reviews
+
+  public static final ReviewEntity REVIEW19 =
+      ReviewEntity.builder()
+          .comment(
+              "I was really looking forward to this but the endless flock of idiots taking selfies"
+                  + " and droning around the place viewing everything through their phone made me"
+                  + " want to vomit.")
+          .rating(2)
+          .user(SeedUsers.USER3)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW25 =
+      ReviewEntity.builder()
+          .comment(
+              "Other the seeing Starry Night up close, the monet painting, "
+                  + "and a painting by Edward Hopper there is "
+                  + "not much else interesting to look at here. "
+                  + "Everything else in the museum is tasteless modern art. ")
+          .rating(2)
+          .user(SeedUsers.USER4)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW26 =
+      ReviewEntity.builder()
+          .comment(
+              "The permanent collection is wonderful and all, but the treatment of patrons was a"
+                  + " joke…")
+          .rating(2)
+          .user(SeedUsers.USER5)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW27 =
+      ReviewEntity.builder()
+          .comment(
+              "It was very disappointing that they “sold” us tickets for the same evening (first"
+                  + " Friday of the month) that it was a “pay what you wish” hour. The museum was"
+                  + " crazy busy. We couldn’t get close to a lot of famous art work because of the"
+                  + " crowd.")
+          .rating(3)
+          .user(SeedUsers.USER6)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW15 =
+      ReviewEntity.builder()
+          .comment(
+              "It was worth it. Art works from amazing and highly recognized artists,"
+                  + " hundreds of different paintings to see… I really enjoyed it,"
+                  + " I can even say one of the best art museums in new York. ")
+          .rating(4)
+          .user(SeedUsers.USER7)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW28 =
+      ReviewEntity.builder()
+          .comment(
+              "It was worth it. Art works from amazing and highly recognized artists,"
+                  + " hundreds of different paintings to see… I really enjoyed it,"
+                  + " I can even say one of the best art museums in new York. ")
+          .rating(4)
+          .user(SeedUsers.USER8)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW32 =
+      ReviewEntity.builder()
+          .comment(
+              "This is a must see.. check your coat it's warm inside. Place for umbrellas at the"
+                  + " entrance. Fantastic collections. Nothing else to be said, this is an art"
+                  + " museum that you shouldn't miss.")
+          .rating(4)
+          .user(SeedUsers.USER16)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW23 =
+      ReviewEntity.builder()
+          .comment("I recommend Columbia Museum to everyone!")
+          .rating(5)
+          .user(SeedUsers.USER9)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW11 =
+      ReviewEntity.builder()
+          .comment(
+              "Excellent exhibits, can spend all day covering wide range!"
+                  + " Go back again and again to enjoy new exhibits periodically. ")
+          .rating(5)
+          .user(SeedUsers.USER10)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW7 =
+      ReviewEntity.builder()
+          .comment(
+              "My new favorite art museum. There's about 6 levels and over "
+                  + "400+ rooms of art here from sculptures, paintings, to multi-media")
+          .rating(5)
+          .user(SeedUsers.USER11)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW24 =
+      ReviewEntity.builder()
+          .comment(
+              "Absolutely phenomenal museum … so intimate and well-curated. It hurt my soul that it"
+                  + " was free and so wonderfully put together, but it is nearly unknown even to"
+                  + " native New Yorkers such as myself. Please set aside 30 minutes to tour this"
+                  + " gem!")
+          .rating(5)
+          .user(SeedUsers.USER12)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW29 =
+      ReviewEntity.builder()
+          .comment(
+              "We are so happy to have found this museum! Inguna at the front desk is such a gem."
+                  + " If you have trouble getting up the stairs, do not come to this museum. We"
+                  + " visited three times in one week!")
+          .rating(5)
+          .user(SeedUsers.USER13)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW30 =
+      ReviewEntity.builder()
+          .comment(
+              "Absolutely loved this museum! It's FREE and the artwork is incredible! I had never"
+                  + " heard of the artist before coming here but I left as a fan of his work. More"
+                  + " people should know about this!")
+          .rating(5)
+          .user(SeedUsers.USER14)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW31 =
+      ReviewEntity.builder()
+          .comment(
+              "This is an amazing place to visit in NYC. It's so perfectly meditative and the works"
+                  + " of art are deep and meaningful. It glows with a sense of power that is subtle"
+                  + " and beautiful.")
+          .rating(5)
+          .user(SeedUsers.USER15)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW33 =
+      ReviewEntity.builder()
+          .comment("It's a beautiful artsy building but a bit boring for children")
+          .rating(5)
+          .user(SeedUsers.USER17)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW34 =
+      ReviewEntity.builder()
+          .comment(
+              "I have to give it 5 stars. I mean, the collection is insane. This is on my top 3"
+                  + " museums globally. However, it is a lot. You will get super tired and spend a"
+                  + " lot of time if you like art. I would recommend starting from up to down and"
+                  + " taking a break.")
+          .rating(5)
+          .user(SeedUsers.USER18)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
+  public static final ReviewEntity REVIEW35 =
+      ReviewEntity.builder()
+          .comment(
+              "One of the best museums I ever visited. Huge collection. Maybe the only thing that"
+                  + " is missing is a printed map of the floors and relative collections.")
+          .rating(5)
+          .user(SeedUsers.USER19)
+          .location(SeedLocations.LOCATION6)
+          .build();
+
   public static final List<ReviewEntity> REVIEWS =
       List.of(
           REVIEW1, REVIEW2, REVIEW3, REVIEW4, REVIEW5, REVIEW6, REVIEW7, REVIEW8, REVIEW9, REVIEW10,
           REVIEW11, REVIEW12, REVIEW13, REVIEW14, REVIEW15, REVIEW16, REVIEW17, REVIEW18, REVIEW19,
-          REVIEW20, REVIEW21, REVIEW22);
+          REVIEW20, REVIEW21, REVIEW22, REVIEW23, REVIEW24, REVIEW25, REVIEW26, REVIEW27, REVIEW28,
+          REVIEW29, REVIEW30, REVIEW31, REVIEW32, REVIEW33, REVIEW34, REVIEW35);
 
   private SeedReviews() {
     throw new IllegalStateException("Utility class");
